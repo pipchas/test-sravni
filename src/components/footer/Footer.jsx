012e2da5145a761ab6ed8f374c1e201feba5ebfe
WIN-M8PT4S6UVRN\Pip <seva.bulava@mail.ru>
2023-06-07 18:36:56 +0300

    review2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer__panel">
-                <div>Bulava V.V. <a></a></div>
+                <div>Bulava V.V. <a href="https://github.com/pipchas/test-sravni/tree/master">GitHub</a></div>
             </div>
         </footer>
     )
